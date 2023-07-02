44df90240a096b68dd0eb1bb061be25a942ce2d6
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-07-02 10:37:34 -0500

    Uso de Api Dom para mostrar en el HTML

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 
-const httpClient = fetch('https://jsonplaceholder.typicode.com/users');
 
-// httpClient.then( Response => {
-//    // console.log(Response);
-//    Response.json().then( data => {
-//       console.log(data);
-//    });
-// });
 
-httpClient
-      .then( response => response.json())
-      .then( data => console.log(data));
 
+// httpClient
+//       .then( response => response.json())
+//       .then( data => console.log(data));
+
+const finAllUsers = async() => {
+   const response = await fetch('https://jsonplaceholder.typicode.com/users');
+   const users = await response.json();
+   const ul =  document.createElement('ul');
+
+   users.forEach(user => {
+      const li = document.createElement('li');
+      li.innerText =  user.name;
+      ul.append(li);
+      console.log(user.name);
+   });
+
+   document.getElementById('root').append(ul);
+}
+
+finAllUsers();
+// console.log(users);
 console.log('Hola');
